ae505965fa0cafaaec98b94495fcdf29e767b920
Bernardo <bernardsantib@hotmail.com>
2022-07-12 13:14:36 -0500

    1v

--- src/public/worker.js
+++ src/public/worker.js
@@ -6,6 +6,28 @@ self.addEventListener('push', e => {
     console.log('Notification Received a lootttt');
     self.registration.showNotification(data.title, {
         body: data.message,
-        icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Archlinux-icon-crystal-64.svg/1024px-Archlinux-icon-crystal-64.svg.png'
+        icon: 'f.png',
+        badge:'f.png'
     });
+});
+
+self.addEventListener('notificationclick', function(event) {
+    let url = 'http://zonadigitalweb.com:4000/serviflash/servicios_pendientes';
+    event.notification.close(); // Android needs explicit close.
+    event.waitUntil(
+        clients.matchAll({type: 'window'}).then( windowClients => {
+            // Check if there is already a window/tab open with the target URL
+            for (var i = 0; i < windowClients.length; i++) {
+                var client = windowClients[i];
+                // If so, just focus it.
+                if (client.url === url && 'focus' in client) {
+                    return client.focus();
+                }
+            }
+            // If not, then open the target URL in a new window/tab.
+            if (clients.openWindow) {
+                return clients.openWindow(url);
+            }
+        })
+    );
 });
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,14 +3,48 @@ const router = Router();
 
 const webpush = require("../webpush");
 let pushSubscripton;
+const aaa = {
+  endpoint: "https://fcm.googleapis.com/fcm/send/f--2iP_vjj4:APA91bFC_xj66to7wpKkYUipA7YCYsYYIH-cYxRNiYidtBW-O_sfbDVz4fgjYq2Tmnn21c13lhkDrAYdOVv82WEhyCNY7Qk3oU9whUyVy37-PVqWVeCbsFRjrplN5PqpIUkYtRHT6Ooh",
+  expirationTime:null,
+  keys:{
+    p256dh:'BBPowO7UjgRV0oYwcInZJjahcqghobZzrY18RuSV1ry_ecNrd0NSy1Pv3y_Ilby3dJNphomOVl_pBva6luXTUlc',
+    auth:'XVxGJzqhzuG-D46GXDoNLg'
+  }
+}
 
 router.post("/subscription", async (req, res) => {
   pushSubscripton = req.body;
-  console.log(pushSubscripton);
-
   // Server's Response
   res.status(201).json();
 });
+
+
+router.get("/new:id/:pas", async (req, res) => {
+  const  {id,pas}  = req.params;
+
+  if (pas==55765412) {
+   const payload = JSON.stringify({
+    title: 'Se registró un nuevo servicio',
+    message:'El folio de la orden de servicio es '+id 
+  });
+
+
+
+  try {
+    await webpush.sendNotification(aaa, payload);
+  } catch (error) {
+    console.log(error);
+  }
+
+  }
+
+  res.redirect('http://zonadigitalweb.com:4000/serviflash/servicios_pendientes')
+
+
+});
+
+
+
 
 router.post("/new-message", async (req, res) => {
   const { message } = req.body;
@@ -19,6 +53,8 @@ router.post("/new-message", async (req, res) => {
     title: "My Custom Notification",
     message 
   });
+
+  console.log(aaa)
   res.status(200).json();
   try {
     await webpush.sendNotification(pushSubscripton, payload);
